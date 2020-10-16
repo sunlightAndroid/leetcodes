cb7c091fca9ba8d18653dd9acf1259642bb1690d
戈传光 <1944633835@qq.com>
2020-10-16 19:53:41 +0800

    第一次提交

---
+++ LettCodeSample/src/com/eric/question/ShuffleArray.java
@@ -0,0 +1,35 @@
+package com.eric.question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShuffleArray {
+	
+	
+	public static void main(String[] args) {
+
+	}
+	
+	
+	 public static int[] shuffle(int[] nums, int n) {
+	      int[] result = new int[nums.length];
+			List<Integer> list= new ArrayList<>();
+			
+			for (int i = 0; i < nums.length; i++) {
+				 if( i <= n-1) {
+					int xn = nums[i];
+					int yn =nums[i+n]; 
+					list.add(xn);
+					list.add(yn);
+				
+		
+				 }
+			}
+			
+			for (int i = 0; i < list.size(); i++) {
+				result[i] = list.get(i);
+			}
+			
+			return result;
+	    }
+}
