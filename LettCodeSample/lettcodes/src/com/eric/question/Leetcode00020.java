a1d173993aade1ca05bbd9d5ef0766c35f2b617c
戈传光 <>
2020-12-14 14:28:46 +0800

    删除多余代码

--- LettCodeSample/lettcodes/src/com/eric/question/Leetcode00020.java
+++ LettCodeSample/lettcodes/src/com/eric/question/Leetcode00020.java
@@ -29,7 +29,6 @@ public class Leetcode00020 {
 	public static class Test {
 		public static void main(String[] args) {
 			
-			// (）））））
 //			String s = "()[]{}";
 //			String s = "]";
 //			String s = "(])";
